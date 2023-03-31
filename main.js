480d08685344cbeb5cae544ce857dfd6d7410ecc
Yash Raj <Yash@google.com>
2023-04-01 00:57:58 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,57 @@
+document.getElementById("input").addEventListener("input", () => {
+  morseToEnglish(input.value);
+});
+
+function morseToEnglish(str) {
+  let morsecode = [
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--..",
+  ];
+  if (str[0] == "." || str[0] == "-") {
+    console.log(
+      "You have inputted morse code, program will convert it back to English"
+    );
+    let text = [];
+    for (let elem of str.split(" ")) {
+      if (elem == "#") text.push(" ");
+      else text.push(String.fromCharCode(morsecode.indexOf(elem) + 97));
+    }
+    console.log(text);
+    document.getElementById("para").textContent = text.join("");
+  } else {
+    console.log(
+      "You have inputted text string, program will convert it to Morse code"
+    );
+    let morse = [];
+    for (let elem of str.split("")) {
+      if (elem !== " ")
+        morse.push(morsecode[elem.toLowerCase().charCodeAt(0) - 97]);
+      else morse.push(elem + "#");
+    }
+    document.getElementById("para").textContent = morse.join(" ");
+  }
+}
